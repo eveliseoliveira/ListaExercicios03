e6d9d80dbe6fe49e7211de6f05b9aefbd1bbd55c
Evelise Oliveira <eveliseoliveira.s@gmail.com>
2022-02-10 11:13:52 -0300

    Adcionei a impressão da sequência de valores do cálculo fatorial.

--- Exercicio05.ConsoleApp/Program.cs
+++ Exercicio05.ConsoleApp/Program.cs
@@ -13,12 +13,24 @@ namespace Exercicio05.ConsoleApp
             Console.WriteLine("Insira um número:");
             numero_valor = int.Parse(Console.ReadLine());
 
-            if (numero_valor < 0)
+            if(numero_valor != 0)
+                Console.Write(numero_valor + "! = ");
+
+            if (numero_valor <= 0)
                 Console.WriteLine("Váriavel inválida!");
+
             else
-                for(contador = numero_valor; contador >= 1; contador--)
+                for (contador = numero_valor; contador >= 1; contador--)
+                {
                     fatorial *= contador;
-                    Console.Write("O fatorial de " + numero_valor + " é " + fatorial);
+                    if (contador != 1)
+                        Console.Write(contador + " x ");
+
+                    else
+                        Console.Write(contador);
+                }
+            if(numero_valor != 0)
+                Console.Write(" = " + fatorial);
 
             Console.ReadLine();
         }
